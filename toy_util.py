ba9fecb2b0b9f3797ef6fdff79a22d6fb7f1779e
DongkeyU99 <dkshin99@gmail.com>
2023-11-11 21:46:11 +0900

    Update Toy_Util

--- toy_util.py
+++ toy_util.py
@@ -1,8 +1,36 @@
-def kernel_estimate():
+def bayesian_update_K1():
     pass
 
-def bayesian_updata():
+def kernel_estimate_K1():
+    pass
+
+def kernel_estimate_K2():
+    pass
+
+def kernel_estimate_K1_conv_K2():
+    pass
+
+def prior_K1():
+    pass
+
+def prior_K2():
+    pass
+
+def prior_K1_conv_K2():
+    pass
+
+def prior_global():
+    pass
+
+def prior_local():
+    pass
+
+def prior_update():
+    pass
+
+def bayesian_inference():
     pass
 
 def train():
+    ##optimizer
     pass

ef407eb017486a9befc02bae6853e230f9b0be0e
Rexchue <terry8737515@gmail.com>
2023-11-26 16:26:40 +0900

    optimization modified

--- optimizer.py
+++ optimizer.py
@@ -1,12 +1,12 @@
-import numpy
-import numpy.fft.fft as fft
+import numpy as np
+import numpy.fft as fft
 from utils import compute_gradient, psf2otf
 
 class Optimizer():
   def __init__(self, kernel, image, likelihood,sigma, max_iterations = 100):
     self.I = image
     
-    self.f = kernal
+    self.f = kernel
     self.L = likelihood
     self.Psi = None
     
@@ -25,38 +25,38 @@ class Optimizer():
     # self.lambda_1 = 1/tau
     # self.lambda_2 = 1/(sigma**2*tau)
     
-  def omega(q):
-    return 1/((zeta_0**2)*tau*(2**q))
+  def omega(self, q):
+    return 1/((self.zeta_0**2)*self.tau*(2**q))
     
-  def conj_fft(array): # conj(FFT) operator
+  def conj_fft(self, array): # conj(FFT) operator
     return np.conj(np.fft.fft(array))
   
 
-  def gradient_filter(type):
+  def gradient_filter(self, type):
     #TODO
     
-  def update_Psi():
+  def update_Psi(self):
     # TODO
     
-  def update_L():
+  def update_L(self):
     gradients = ['x','y','xx','xy','yy']
-    gradient_filters = gradient_filter(gradients)
-    self.Psi = [compute_gradient(likelihood, type='x'),compute_gradient(likelihood, type='y')]
+    gradient_filters = self.gradient_filter(gradients)
+    self.Psi = [compute_gradient(self.likelihood, type='x'),compute_gradient(self.likelihood, type='y')]
     
-    grad_x = gradient_filter('x')
-    grad_y = gradient_filter('y')
+    grad_x = self.gradient_filter('x')
+    grad_y = self.gradient_filter('y')
     [Psi_x, Psi_y] = self.Psi
 
-    Delta = np.sum(omega(len(gradients))*conj_fft(gradient_filters)*gradient_filters)     # q=1 for x y, q=2 for xx xy yy
+    Delta = np.sum(self.omega(len(gradients))*self.conj_fft(gradient_filters)*gradient_filters)     # q=1 for x y, q=2 for xx xy yy
 
-    numer = np.sum( conj_fft(kernel)*fft(image)*Delta + gamma*conj_fft(grad_x)*fft(phi_x) + gamma*conj_fft(grad_y)*fft(phi_y) )
-    numer = np.sum( conj_fft(kernel)*fft(kernel)*Delta + gamma*conj_fft(grad_x)*fft(grad_x) + gamma*conj_fft(grad_y)*fft(grad_y) )
+    denom = np.sum(self.conj_fft(self.f)*fft(self.I)*Delta + self.gamma*self.conj_fft(grad_x)*fft(self.Psi[0]) + self.gamma*self.conj_fft(grad_y)*fft(self.Psi[1]) )
+    numer = np.sum(self.conj_fft(self.f)*fft(self.I)*Delta + self.gamma*self.conj_fft(grad_x)*fft(grad_x) + self.gamma*self.conj_fft(grad_y)*fft(grad_y))
     new_L = np.fft.ifft(numer/denom)
     
     self.delta_L = new_L - self.L
     self.L = new_L
   
-  def update_f():  
+  def update_f(self):  
     #TODO
     self.delta_f = new_f - self.f
     self.f = new_f    
@@ -67,8 +67,8 @@ class Optimizer():
       # Inner loop to optimize L
       while True:
         # Update Ψ and compute L
-        update_Psi()
-        update_L()
+        self.update_Psi()
+        self.update_L()
         if  np.linalg.norm(self.delta_L) < 1e-5 and np.linalg.norm(self.delta_Psi) < 1e-5:
           break
       # Update f

73ea06573354c59717e434e4d11bbd3a4d30b539
tlswhdgns0406 <tlswhdgns0406@gmail.com>
2023-11-26 15:29:31 +0900

    local_prior

--- local_prior.py
+++ local_prior.py
@@ -1,28 +1,36 @@
-#def std_convoluted(image, N):
-#im = np.array(image, dtype=float)
-#     im2 = im**2
-    #     ones = np.ones(im.shape)
-    
-    #     kernel = np.ones((2*N+1, 2*N+1))
-    #     s = scipy.signal.convolve2d(im, kernel, mode="same")
-    #     s2 = scipy.signal.convolve2d(im2, kernel, mode="same")
-    #     ns = scipy.signal.convolve2d(ones, kernel, mode="same")
-    
-#     return np.sqrt((s2 - s**2 / ns) / ns)
+import numpy as np
+import scipy
+from utils import compute_gradient
+from scipy.stats import multivariate_normal
+import cv2
 
 def smooth_region(img, kernel_size, threshold):
     local_std = np.zeros(img.shape)
     std_kernel = np.ones((kernel_size, kernel_size))
     im = np.array(img, dtype = float)
     im2 = im**2
-    ones = np.ones((im.shape[1], im.shape[2]))
+    ones = np.ones((im.shape[0], im.shape[1]))
 
     for i in range(3):
-        s = scipy.signal.convolve2d(im[i], std_kernel, mode="same")
-        s2 = scipy.signal.convolve2d(im2[i], std_kernel, mode="same")
+        s = scipy.signal.convolve2d(im[:,:,i], std_kernel, mode="same")
+        s2 = scipy.signal.convolve2d(im2[:,:,i], std_kernel, mode="same")
         ns = scipy.signal.convolve2d(ones, std_kernel, mode="same")
-        local_std[i] = np.sqrt((s2 - s**2 / ns) / ns)
+        local_std[:,:,i] = np.sqrt((s2 - s**2 / ns) / ns)
         
-    region = (local_std[0] > threshold[0]) & (local_std[1] > threshold[1]) & (local_std[2] > threshold[2])
+    region = (local_std[:,:,0] < threshold[0]) & (local_std[:,:,1] < threshold[1]) & (local_std[:,:,2] < threshold[2])
 
     return region # True면 Omega에 들어가는 pixel (img.shape[1], img.shape[2])
+
+def prior_local(latent_img, img, kernel_size, sigma_1, threshold = np.array([5, 5, 5])):
+    region = smooth_region(img, kernel_size, threshold)
+    idx = np.where(region == True)
+
+    delta_grad_x = compute_gradient(latent_img, cv2.CV_64F, 1, 0) - compute_gradient(img, cv2.CV_64F, 1, 0)
+    delta_grad_y = compute_gradient(latent_img, cv2.CV_64F, 0, 1) - compute_gradient(img, cv2.CV_64F, 0, 1)
+
+    px = multivariate_normal.logpdf(delta_grad_x[idx].flatten(), mean=0, cov=sigma_1)
+    py = multivariate_normal.logpdf(delta_grad_y[idx].flatten(), mean=0, cov=sigma_1)
+
+    log_likelihood = np.sum(px) + np.sum(py)
+
+    return log_likelihood

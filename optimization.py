e350cb848450f7116eb1a0344a46a7439a40d76e
OXcarXierra <littlestein@snu.ac.kr>
2023-11-26 15:16:35 +0900

    fix: add gradient_filter todo

--- optimization.py
+++ optimization.py
@@ -1,6 +1,6 @@
 import numpy
 import numpy.fft.fft as fft
-from utils import psf2otf
+from utils import compute_gradient, psf2otf
 
 class optimizer():
   def __init__(self, likelihood, zeta_0, gamma, tau, sigma, image, kernel):
@@ -16,21 +16,21 @@ class optimizer():
   def conj_fft(array): # conj(FFT) operator
     return np.conj(np.fft.fft(array))
   
-  def compute_gradient(type):
+  def gradient_filter(type):
     #TODO
     
   def likelihood_update():
     gradients = ['x','y','xx','xy','yy']
-    gradient_filters = compute_gradient(gradients)
+    gradient_filters = gradient_filter(gradients)
+    phi_x = compute_gradient(likelihood, 'x')
+    phi_y = compute_gradient(likelihood, 'y')
+    grad_x = gradient_filter('x')
+    grad_y = gradient_filter('y')
     
-    Delta = np.sum(omega(len(gradients))*np.dot(conj_fft(gradient_filters),gradient_filters))
-    # q=1 for x y, q=2 for xx xy yy
-    
-    grad_x = compute_gradient('x')
-    grad_y = compute_gradient('y')
+    Delta = np.sum(omega(len(gradients))*conj_fft(gradient_filters)*gradient_filters)     # q=1 for x y, q=2 for xx xy yy
 
-    numer = np.sum(np.dot(conj_fft(kernel),fft(image),Delta)+gamma*np.dot(conj_fft(grad_x),fft(phi_x))+gamma*np.dot(conj_fft(grad_y),fft(phi_y)))
-    denom = np.sum(np.dot(conj_fft(kernel),fft(kernel),Delta)+gamma*np.dot(conj_fft(grad_x),fft(grad_x))+gamma*np.dot(conj_fft(grad_y),fft(grad_y)))
+    numer = np.sum( conj_fft(kernel)*fft(image)*Delta + gamma*conj_fft(grad_x)*fft(phi_x) + gamma*conj_fft(grad_y)*fft(phi_y) )
+    numer = np.sum( conj_fft(kernel)*fft(kernel)*Delta + gamma*conj_fft(grad_x)*fft(grad_x) + gamma*conj_fft(grad_y)*fft(grad_y) )
     self.new_L = np.fft.ifft(numer/denom)
     return new_L
   

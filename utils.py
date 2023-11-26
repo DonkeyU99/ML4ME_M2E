b23bca11f2797f046ca000e577f3a7ca9305fd3b
tlswhdgns0406 <tlswhdgns0406@gmail.com>
2023-11-26 22:49:23 +0900

    utils.toeplitz_matrix

--- utils.py
+++ utils.py
@@ -15,3 +15,15 @@ def compute_gradient(source, type='x', ddepth = cv2.CV_64F):
       
 def Phi_func(x, threshold, k = 2.7, a = 6.1e-4, b = 5):
     return np.where(x <= threshold, -k*x, -b-a*x**2)
+
+def toeplitz_matrix(input, kernel_size):
+    height, width = input.shape
+    p = int((kernel_size - 1) / 2)
+    input = np.pad(input, ((p,p),(p,p)), 'constant', constant_values=0)
+    result = np.zeros((height*width, kernel_size**2))
+    for i in range(height):
+        for j in range(width):
+            local_mat = input[i:i+kernel_size, j:j+kernel_size]
+            local_mat = local_mat.flatten()
+            result[i*width + j] = local_mat
+    return result

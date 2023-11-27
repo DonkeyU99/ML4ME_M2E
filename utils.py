3abd1e4a646fe88f047c80f652b741ccf5f6df21
tlswhdgns0406 <tlswhdgns0406@gmail.com>
2023-11-27 21:51:46 +0900

    toeplitz_mat to 3d

--- utils.py
+++ utils.py
@@ -17,13 +17,15 @@ def Phi_func(x, threshold, k = 2.7, a = 6.1e-4, b = 5):
     return np.where(x <= threshold, -k*x, -b-a*x**2)
 
 def toeplitz_matrix(input, kernel_size):
-    height, width = input.shape
+    height, width, channel = input.shape
     p = int((kernel_size - 1) / 2)
-    input = np.pad(input, ((p,p),(p,p)), 'constant', constant_values=0)
-    result = np.zeros((height*width, kernel_size**2))
-    for i in range(height):
-        for j in range(width):
-            local_mat = input[i:i+kernel_size, j:j+kernel_size]
-            local_mat = local_mat.flatten()
-            result[i*width + j] = local_mat
+    result = np.zeros((height*width, kernel_size**2, channel))
+    for k in range(channel):
+        input_c = np.pad(input[:, :, k], ((p,p),(p,p)), 'constant', constant_values=0)
+        for i in range(height):
+            for j in range(width):
+                local_mat = input_c[i:i+kernel_size, j:j+kernel_size]
+                local_mat = local_mat.flatten()
+                result[i*width + j,:,k] = local_mat
+
     return result

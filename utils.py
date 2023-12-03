94b7aaf6c1a092d19b3b4d9ca87c4df58a934880
DongkeyU99 <dkshin99@gmail.com>
2023-12-03 22:43:03 +0900

    바꾼 것

--- utils.py
+++ utils.py
@@ -3,18 +3,20 @@ import numpy as np
 
 def compute_gradient(source, type='x', ddepth = cv2.CV_64F):
     if(type == 'x'):
-        return cv2.Sobel(source,ddepth,dx=1,dy=0)
+        a = cv2.Sobel(source,ddepth,dx=1,dy=0)
     if(type == 'xx'):
-        return cv2.Sobel(source,ddepth,dx=2,dy=0)
+        a = cv2.Sobel(source,ddepth,dx=2,dy=0)
     if(type == 'y'):
-        return cv2.Sobel(source,ddepth,dx=0,dy=1)
+        a = cv2.Sobel(source,ddepth,dx=0,dy=1)
     if(type == 'yy'):
-        return cv2.Sobel(source,ddepth,dx=0,dy=2)
+        a = cv2.Sobel(source,ddepth,dx=0,dy=2)
     if(type == 'xy'):
-        return cv2.Sobel(source,ddepth,dx=1,dy=1)
-      
+        a = cv2.Sobel(source,ddepth,dx=1,dy=1)
+        
+    return a
+
 def Phi_func(x, threshold, k = 2.7, a = 6.1e-4, b = 5):
-    return np.where(x <= threshold, -k*x, -b-a*x**2)
+    return np.where(np.abs(x) <= threshold, -k*np.abs(x), -b-a*x**2)
 
 def toeplitz_matrix(input, kernel_size):
     height, width, channel = input.shape

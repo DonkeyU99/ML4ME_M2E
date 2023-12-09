dbb0e2f31d232b02c52a3f964d394cbb801aaa45
DongkeyU99 <dkshin99@gmail.com>
2023-12-09 16:30:04 +0900

    Fix SVD

--- svd.py
+++ svd.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 def load_data(blur_path, sharp_path):
     blur = cv2.imread(blur_path, cv2.IMREAD_GRAYSCALE) # uint8
-    blur_img = cv2.resize(blur, (300, 300)) # uint8
+    blur_img = cv2.resize(blur, (blur.shape[1],blur.shape[0])) # uint8
     
     sharp = cv2.imread(sharp_path, cv2.IMREAD_GRAYSCALE)  # uint8
-    sharp_img = cv2.resize(sharp, (300, 300))  # uint8
+    sharp_img = cv2.resize(sharp, (sharp.shape[1],blur.shape[0]))  # uint8
     
     return blur_img, sharp_img
 
@@ -16,23 +16,28 @@ class Kernel:
         self.blur = blur_img
         self.sharp = sharp_img
         self.k = ker_size
-    
+        self.s_size =(1000,1000)
+ 
     def Calculate(self):
-        F_blur = np.fft.fft2(blur_img)
-        F_sharp = np.fft.fft2(sharp_img)
+        pad_size = int(self.k/2)
+        F_blur = np.fft.fft2(np.pad(self.blur,((pad_size,pad_size),(pad_size,pad_size))),(2000,2000))
+        F_sharp = np.fft.fft2(np.pad(self.sharp,((pad_size,pad_size),(pad_size,pad_size))),(2000,2000))
 
         F_kernel = F_blur / F_sharp
-        kernel = np.fft.ifft2(F_kernel, (self.k, self.k))
+        kernel = np.fft.ifft2(F_kernel, (2000,2000))
+        kernel = kernel[:self.k,:self.k]
         return kernel
     
     def reverse(self, kernel, sharp):
         # p = int((sharp.shape[0]-kernel.shape[0])/2)
         # kernel = np.pad(kernel, ((p,p),(p,p)), 'constant', constant_values = 0)
-        F_kernel = np.fft.fft2(kernel, (sharp.shape[0], sharp.shape[1]))
+        F_kernel = np.fft.fft2(kernel,(2000,2000))
         
-        F_sharp = np.fft.fft2(sharp)
+        pad_size = int(self.k/2)
+        F_sharp = np.fft.fft2(np.pad(sharp,((pad_size,pad_size),(pad_size,pad_size))),(2000,2000))
         F_blur = F_sharp * F_kernel
-        blur = np.fft.ifft2(F_blur)
+        blur = np.fft.ifft2(F_blur,(2000,2000))
+        blur = blur[:sharp.shape[0],:sharp.shape[1]]
         return blur
 
 
@@ -46,7 +51,7 @@ plt.savefig(f"./data/test_dataset/data.png")
 
 plt.show()
 
-a = Kernel(blur_img, sharp_img, 9)
+a = Kernel(blur_img, sharp_img, 30)
 kernel = a.Calculate()
 plt.imshow(np.abs(kernel), cmap = 'gray')
 plt.savefig(f"./data/test_dataset/kernel.png")

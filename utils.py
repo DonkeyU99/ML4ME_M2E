3e802301fb8393ca5029d22e99aef02b8d03d007
Rexchue <terry8737515@gmail.com>
2023-12-09 13:51:34 +0900

    utils modified

--- utils.py
+++ utils.py
@@ -19,15 +19,14 @@ def Phi_func(x, threshold, k = 2.7, a = 6.1e-4, b = 5):
     return np.where(np.abs(x) <= threshold, -k*np.abs(x), -b-a*x**2)
 
 def toeplitz_matrix(input, kernel_size):
-    height, width, channel = input.shape
+    height, width = input.shape
     p = int((kernel_size - 1) / 2)
-    result = np.zeros((channel, height*width, kernel_size**2))
-    for k in range(channel):
-        input_c = np.pad(input[:, :, k], ((p,p),(p,p)), 'constant', constant_values=0)
-        for i in range(height):
-            for j in range(width):
-                local_mat = input_c[i:i+kernel_size, j:j+kernel_size]
-                local_mat = local_mat.flatten()
-                result[k, i*width + j,:] = local_mat
+    result = np.zeros(( height*width, kernel_size**2))
+    input_c = np.pad(input[:, :], ((p,p),(p,p)), 'constant', constant_values=0)
+    for i in range(height):
+        for j in range(width):
+            local_mat = input_c[i:i+kernel_size, j:j+kernel_size]
+            local_mat = local_mat.flatten()
+            result[i*width + j,:] = local_mat
 
     return result # (3, height, width) 느낌으로 출력
